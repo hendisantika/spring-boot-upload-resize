bddeb0f413ad186b8945d803bac4e99b431acc36
hendisantika <hendisantika@yahoo.co.id>
2024-04-19 08:00:39 +0700

    Add resizeImage in ImageService 🫸🌀✏️📗🐧🐳

--- src/main/java/id/my/hendisantika/springbootuploadresize/service/ImageService.java
+++ src/main/java/id/my/hendisantika/springbootuploadresize/service/ImageService.java
@@ -1,9 +1,18 @@
 package id.my.hendisantika.springbootuploadresize.service;
 
+import org.apache.commons.io.FilenameUtils;
+import org.imgscalr.Scalr;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 /**
  * Created by IntelliJ IDEA.
@@ -24,4 +33,22 @@ public class ImageService {
     private Integer imageSize;
 
     private final Logger logger = LoggerFactory.getLogger(ImageService.class);
+
+    public boolean resizeImage(File sourceFile) {
+        try {
+            BufferedImage bufferedImage = ImageIO.read(sourceFile);
+            BufferedImage outputImage = Scalr.resize(bufferedImage, imageSize);
+            String newFileName = FilenameUtils.getBaseName(sourceFile.getName())
+                    + "_" + imageSize.toString() + "."
+                    + FilenameUtils.getExtension(sourceFile.getName());
+            Path path = Paths.get(imageFolder, newFileName);
+            File newImageFile = path.toFile();
+            ImageIO.write(outputImage, "jpg", newImageFile);
+            outputImage.flush();
+            return true;
+        } catch (IOException e) {
+            logger.error(e.getMessage(), e);
+            return false;
+        }
+    }
 }
