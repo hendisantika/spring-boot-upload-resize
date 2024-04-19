aae392d2ae9ca955e4cf4f0b400ad2061a5cb1fc
hendisantika <hendisantika@yahoo.co.id>
2024-04-19 08:02:16 +0700

    Add ImageUploadController 🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/springbootuploadresize/controller/ImageUploadController.java
@@ -0,0 +1,27 @@
+package id.my.hendisantika.springbootuploadresize.controller;
+
+import id.my.hendisantika.springbootuploadresize.service.FileUploadService;
+import id.my.hendisantika.springbootuploadresize.service.ImageService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-upload-resize
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 4/19/24
+ * Time: 08:02
+ * To change this template use File | Settings | File Templates.
+ */
+@Controller
+public class ImageUploadController {
+
+    @Autowired
+    private FileUploadService fileUploadService;
+
+    @Autowired
+    private ImageService imageService;
+
+}
