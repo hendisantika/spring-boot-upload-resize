5d834ddaacf8a0b5efd152d3004ab26ab3b329f9
hendisantika <hendisantika@yahoo.co.id>
2024-04-19 07:56:59 +0700

    Create starter project 🫸🌀✏️📗🐧🐳

---
+++ src/test/java/id/my/hendisantika/springbootuploadresize/SpringBootUploadResizeApplicationTests.java
@@ -0,0 +1,13 @@
+package id.my.hendisantika.springbootuploadresize;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootUploadResizeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
