351e987b2d25f8fcff19dceeb51e2539eaabef79
hendisantika <hendisantika@yahoo.co.id>
2024-04-19 08:02:40 +0700

    Add uploadImage in ImageUploadController 🫸🌀✏️📗🐧🐳

--- src/main/java/id/my/hendisantika/springbootuploadresize/controller/ImageUploadController.java
+++ src/main/java/id/my/hendisantika/springbootuploadresize/controller/ImageUploadController.java
@@ -5,6 +5,12 @@ import id.my.hendisantika.springbootuploadresize.service.ImageService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.io.File;
 
 /**
  * Created by IntelliJ IDEA.
@@ -28,5 +34,28 @@ public class ImageUploadController {
     @GetMapping("")
     public String uploadImage() {
         return "uploadImage";
+    }
+
+    @PostMapping("/uploadImage")
+    public String uploadImage(@RequestParam("image") MultipartFile imageFile, RedirectAttributes redirectAttributes) {
+        if (imageFile.isEmpty()) {
+            redirectAttributes.addFlashAttribute("errorMessage", "Please choose file to upload.");
+            return "redirect:/";
+        }
+
+        File file = fileUploadService.upload(imageFile);
+        if (file == null) {
+            redirectAttributes.addFlashAttribute("errorMessage", "Upload failed.");
+            return "redirect:/";
+
+        }
+        boolean resizeResult = imageService.resizeImage(file);
+        if (!resizeResult) {
+            redirectAttributes.addFlashAttribute("errorMessage", "Resize failed.");
+            return "redirect:/";
+        }
+
+        redirectAttributes.addFlashAttribute("successMessage", "File upload successfully.");
+        return "redirect:/";
     }
 }
